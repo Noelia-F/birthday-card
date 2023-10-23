3a582365ca4f9bcfbbec7d9f4c068029f74c2303
Noelia-F <noelia.fronton.g@gmail.com>
2023-10-23 23:25:51 +0200

    feat: add form logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,50 +3,115 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import { useState } from 'react';
+import useForm from '../hooks/useForm';
 
 export default function Home() {
 
-  const defaultInfo = {
-    upperIntro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non.',
-    title: 'Lorem ipsum dolor sit amet',
-    extraHighlightText: '2020',
-    intro: 'Lorem ipsum dolor sit amet.',
-  }
+  const { data, onChangeData, onSubmitForm } = useForm({
+    upperIntro: 'We request your presence at my',
+    title: 'Birthday!',
+    highlightText: '20 years',
+    intro: 'Please join us',
+    contact: 'How do you want to be contacted?',
+    day: '20',
+    monthName: 'april',
+    hour: '4pm',
+    address: 'The Stabans Street 12345',
+  });
 
-  const [info, setInfo] = useState(defaultInfo);
+  const { upperIntro, title, highlightText, intro, contact, day, monthName, hour, address } = data;
 
   return (
     <main className={styles.main}>
       <article className={styles.card}>
         <section className={styles.section}>
-          <img className={styles.logo} src="https://picsum.photos/id/237/100/100" alt='logo' />
-          <p className={styles.text}>{info.upperIntro}</p>
-          <h1 className={styles.title}>{info.title}</h1>
-          <p>{info.extraHighlightText}</p>
+          <p>{highlightText}</p>
         </section>
         <section className={styles.section}>
-          <p className={styles.text}>{styles.intro}</p>
-          <h2 className={styles.subtitle}>20-25 april 2pm-6pm</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <h2 className={styles.subtitle}>contact</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className={styles.text}>{upperIntro}</p>
+          <h1 className={styles.title}>{title}</h1>
+        </section>
+        <section className={styles.section}>
+          <p className={styles.text}>{intro}</p>
+        </section>
+        <section className={styles.section}>
+          <div>
+            <p className={styles.text}>{day}</p>
+            <p className={styles.text}>{monthName}</p>
+          </div>
+          <div>
+            <p className={styles.subtitle}>{hour}</p>
+            <p className={styles.text}>{address}</p>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <p>{contact}</p>
         </section>
       </article>
       <form className={styles.form}>
+        <label>The important thing...</label>
+        <input
+          name="highlightText"
+          value={highlightText}
+          type="highlightText"
+          placeholder="20"
+          onChange={onChangeData} />
         <label>Upper Intro</label>
-        <input type="text" id="fname" name="fname" />
-        <label>Title</label>
-        <input type="text" id="lname" name="lname" />
-        <label>Extra highlight text</label>
-        <input type="text" id="lname" name="lname" />
-        <label>Info Intro</label>
-        <input type="text" id="lname" name="lname" />
-        <label>Info Date</label>
-        <input type="text" id="lname" name="lname" />
-        <label>Info Direction</label>
-        <input type="text" id="lname" name="lname" />
-        <label>Info Contact</label>
-        <input type="text" id="lname" name="lname" />
+        <input
+          name="upperIntro"
+          value={upperIntro}
+          type="upperIntro"
+          placeholder="We request your presence at my"
+          onChange={onChangeData} />
+        <label>Name of the event</label>
+        <input
+          name="title"
+          value={title}
+          type="title"
+          placeholder="Birthday"
+          onChange={onChangeData} />
+        <label>Brief description</label>
+        <input
+          name="intro"
+          value={intro}
+          type="intro"
+          placeholder="Please join us"
+          onChange={onChangeData} />
+        <label>Date</label>
+        <input
+          name="day"
+          value={day}
+          type="day"
+          placeholder="Write your day..."
+          onChange={onChangeData} />
+        <label>Month</label>
+        <input
+          name="monthName"
+          value={monthName}
+          type="monthName"
+          placeholder="Write your month..."
+          onChange={onChangeData} />
+        <label>Hour</label>
+        <input
+          name="hour"
+          value={hour}
+          type="hour"
+          placeholder="Write your hour..."
+          onChange={onChangeData} />
+        <label>Address</label>
+        <input
+          name="address"
+          value={address}
+          type="address"
+          placeholder="Write your address..."
+          onChange={onChangeData} />
+        <label>Contact</label>
+        <input
+          name="contact"
+          value={contact}
+          type="contact"
+          placeholder="Write your contact..."
+          onChange={onChangeData} />
       </form>
     </main>
     /* <main className={styles.main}>
---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+interface FormData {
+  upperIntro: string;
+  title: string;
+  highlightText: string;
+  intro: string;
+  contact: string;
+  day: string;
+  monthName: string;
+  hour: string;
+  address: string;
+};
+
+const useForm = (formState: FormData) => {
+  const [data, setData] = useState(formState)
+
+  const onChangeData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+
+    setData({ ...data, [name]: value })
+  }
+
+  const onSubmitForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    console.log('data', data)
+  }
+
+  return {
+    data,
+    onChangeData,
+    onSubmitForm,
+  }
+}
+
+export default useForm;
